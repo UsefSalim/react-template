fda786bfcec59eb6dffb70755fa2b5d22b4027ff
xelorNpm <salimguithub@gmail.com>
2022-05-04 22:36:44 +0000

    add docker test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Logo from 'assets/images/Logo.svg';
+import React from 'react';
 
 const Home = () => (
   <div
@@ -10,13 +10,7 @@ const Home = () => (
       alignItems: 'center',
     }}
   >
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <img src={Logo} style={{ width: 300 }} alt="Sobrus" />
     </div>
   </div>
